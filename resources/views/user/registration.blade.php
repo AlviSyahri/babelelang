88be021d63096a297ca19495f7d6743b31eceff3
Felix Winangun <felixwinangun@ymail.com>
2018-03-24 22:21:20 +0700

    register view

--- resources/views/user/registration.blade.php
+++ resources/views/user/registration.blade.php
@@ -1,37 +1,42 @@
 @extends('layout.main')
 @section('content')
 
- <!-- Inner Page Banner Area End Here -->
-        <!-- Login Registration Page Area Start Here -->
-        <div class="login-registration-page-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                       
-                            
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                        <div class="login-registration-field">
-                            <h2 class="cart-area-title">Register</h2>
-                            <form enctype="true">
-                                <label>Full Name </label>
-                                <input type="text" placeholder="Full name" />
-                                <label>Email address </label>
-                                <input type="text" placeholder="E-mail" />
-                                <label>Password </label>
-                                <input type="password" placeholder="Password" />
-                                <label>Foto KTP </label>
-                                <input type="file" />                                
-                                <button class="btn-send-message disabled" type="submit" value="Login">Register</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+<!-- Inner Page Banner Area End Here -->
+<!-- Login Registration Page Area Start Here -->
+<div class="login-registration-page-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+
+
             </div>
         </div>
-        <!-- Login Registration Page Area End Here -->
-        <!-- Footer Area Start Here -->
-        <footer>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="login-registration-field">
+                <h2 class="cart-area-title">Register</h2>
+                <form enctype="true" method="POST" action="/registration">
+                    @csrf
+                    <label>Full Name </label>
+                    <input type="text" placeholder="Full name" name="nama" />
+                    <label>Email address </label>
+                    <input type="text" placeholder="E-mail" name="email" />
+                    <label>Password </label>
+                    <input type="password" placeholder="Password" name="password" />
+                    <label>Password </label>
+                    <input type="password" placeholder="Password Confirmation" name="password_confirmation" />
+                    <label>Foto KTP </label>
+                    <input type="file" name="image1" />  
+                    <label>Foto Profil </label>
+                    <input type="file" name="image" />
+                    <button class="btn-send-message disabled" type="submit" value="Register">Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- Login Registration Page Area End Here -->
+<!-- Footer Area Start Here -->
+<footer>
 
-@stop
+    @stop
