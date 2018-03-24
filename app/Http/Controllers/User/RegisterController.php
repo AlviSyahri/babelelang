b0ec33120475c8b0baea0d319d2b4d0ee0a32d5f
DESKTOP-TL4NPDO\Fenry <Fenry.photography@gmail.com>
2018-03-24 22:12:52 +0700

    Register Controller

--- app/Http/Controllers/User/RegisterController.php
+++ app/Http/Controllers/User/RegisterController.php
@@ -41,19 +41,21 @@ class Register extends Controller
     		'password_confirmation'		 =>'required|min:6',
     		'jenis_kelamin.required'	 =>'required'
     	],$message);
-          if($validator->fails()){
+
+        if($validator->fails()){
           return redirect()->back()
                      ->withErrors($validator)
                      ->withInput();
         }
         else{
             $filename = "";
+            $filename1= "";
             $path     ='User/Profile/';
             $path1    ='User/Ktp/';
-            //image
+
             $image =  $request->file('image');
             $image1=  $request->file('image1');
-            if($request->hasfile('image')&& $request->hasfile('image1')){
+            if($request->hasfile('image') && $request->hasfile('image1')){
                 if($imagevalidator->fails()){
                     return redirect()
                         ->back()
@@ -67,15 +69,19 @@ class Register extends Controller
                     $image1->move($path1, $filename1);
                 }
             }
-      $data = new User; 
-      $data->nama=$request->nama;
-      $data->alamat=$request->alamat;
-      $data->email=$request->email;
-      $data->password=Hash::make($request->password);
-      $data->no_telepon=$request->no_telepon;
-      $data->dob=$request->dob;
-      $data->jenis_kelamin=$request->jenis_kelamin;
-      
-
-   	}
+            $data = new User; 
+            $data->nama=$request->nama;
+            $data->alamat=$request->alamat;
+            $data->email=$request->email;
+            $data->password=Hash::make($request->password);
+            $data->no_telepon=$request->no_telepon;
+            $data->dob=$request->dob;
+            $data->jenis_kelamin=$request->jenis_kelamin;
+            $data->foto=$filename;
+            $data->foto_ktp=$filename1;
+            $data->user_activation='0';
+            $data->save();
+            Auth::login($data);
+            return view('home');
+        }
 }
