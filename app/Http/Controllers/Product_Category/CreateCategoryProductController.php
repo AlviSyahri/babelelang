357a93a251988812d289799d768e49b712c813e5
Fenry <Fenry.photography@gmail.com>
2018-03-31 17:37:37 +0700

    add category product  controller

---
+++ app/Http/Controllers/Product_Category/CreateCategoryProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Product_Category;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\Product_Category;
+
+
+class CreateCategoryProductController extends Controller
+{
+    public function CreateCategory(Request $request){
+    	$message = [
+            'product_category_name.required'	=> 'Nama kategori tidak boleh kosong'
+        ];
+        $validator = Validator::make($request->all(),[
+        	'product_category_name'		=>'required'
+        ],$message);
+
+            $category = new Product_Category;
+        	$category->product_category_name=$request->product_category_name;
+        	$category->flag_delete=0;
+            $category->save();
+            
+        return redirect('/admin');
+	}    
+}
