ba0a4da970e0652eb42019519d2a0c9de6a6353f
Dharmawan <dharmawanrahadi@gmail.com>
2018-04-08 20:04:00 +0700

    ini about baru di coba

---
+++ resources/views/about.blade.php
@@ -0,0 +1,253 @@
+@extends('admin.layout.mainLogin')
+@section('content')
+
+
+
+
+<!-- Mobile Menu Area Start Here -->
+                    <div class="mobile-menu-area">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="mobile-menu">
+                                        <nav id="dropdown">
+                                            <ul>
+                                                <li class="active"><a href="#">Home</a>
+                                                    <ul>
+                                                        <li><a href="index.html">Home 1</a></li>
+                                                        <li><a href="index2.html">Home 2</a></li>
+                                                        <li><a href="index3.html">Home 3</a></li>
+                                                        <li><a href="index4.html">Home 4</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="about.html">About</a></li>
+                                                <li><a href="#">Blog</a>
+                                                    <ul>
+                                                        <li><a href="blog.html">Blog</a></li>
+                                                        <li><a href="single-blog.html">Single Blog</a></li>
+                                                        <li class="has-child-menu"><a href="#">Demo</a>
+                                                            <ul class="thired-level">
+                                                                <li><a href="#">Demo 1</a></li>
+                                                                <li><a href="#">Demo 2</a></li>
+                                                            </ul>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="#">Portfolio</a>
+                                                    <ul>
+                                                        <li><a href="portfolio1.html">Portfolio 1</a></li>
+                                                        <li><a href="portfolio2.html">Portfolio 2</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="#">Shop</a>
+                                                    <ul>
+                                                        <li><a href="shop1.html">Shop 1</a></li>
+                                                        <li><a href="shop2.html">Shop 2</a></li>
+                                                        <li><a href="shop3.html">Shop 3</a></li>
+                                                        <li><a href="shop4.html">Shop 4</a></li>
+                                                        <li><a href="shop5.html">Shop 5</a></li>
+                                                        <li><a href="shop6.html">Shop 6</a></li>
+                                                        <li><a href="shop7.html">Shop 7</a></li>
+                                                        <li><a href="product-details1.html">Shop Details 1</a></li>
+                                                        <li><a href="product-details2.html">Shop Details 2</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="#">Pages</a>
+                                                    <ul>
+                                                        <li><a href="login-registration.html">Login Registration</a></li>
+                                                        <li><a href="my-account.html">My Account</a></li>
+                                                        <li><a href="cart.html">Cart</a></li>
+                                                        <li><a href="wishlist.html">Wishlist</a></li>
+                                                        <li><a href="check-out.html">Check Out</a></li>
+                                                        <li><a href="order-history.html">Order History</a></li>
+                                                        <li><a href="order-details.html">Order Details</a></li>
+                                                        <li><a href="404.html">404</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="contact.html">Contact</a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu Area End Here -->
+                </div>
+            </div>
+        </header>
+        <!-- Header Area End Here -->
+        <!-- Inner Page Banner Area Start Here -->
+        <div class="inner-page-banner-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="breadcrumb-area">
+                            <h1>About Us</h1>
+                            <ul>
+                                <li><a href="#">Home</a> /</li>
+                                <li>About</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Inner Page Banner Area End Here -->
+        <!-- About Us Page Area Start Here -->
+        <div class="about-us-page-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                        <div class="about-us-page-left">
+                            <a href="#"><img src="img/about-us.jpg" alt="about" class="img-responsive"></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+                        <div class="about-us-page-right">
+                            <h2>Who We Are</h2>
+                            <p>
+                                <span>Morem ipsum dolor sit amet, consectetur adipisci Reit, sed do eiusmod tempor 
+                                    incididuntut magna aliquaoe velit esse.</span>
+                            </p>
+                            <p>Tummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.</p>
+                            <p>Hmmy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release ver since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About Us Page Area End Here -->
+        <!-- Expert Team Area Start Here -->
+        <div class="expert-team-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="section-title">
+                            <span class="title-bar-left"></span>
+                            <h2>Expert Team</h2>
+                            <span class="title-bar-right"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="expert-team-box">
+                            <ul class="expert-team-social">
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-pinterest"></i></a></li>
+                            </ul>
+                            <div class="team-img-holder">
+                                <a href="#"><img src="img/team/1.png" alt="team" class="img-responsive"></a>
+                            </div>
+                            <div class="team-content-holder">
+                                <h3><a href="#">Jessy Rio</a></h3>
+                                <p>Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="expert-team-box">
+                            <ul class="expert-team-social">
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-pinterest"></i></a></li>
+                            </ul>
+                            <div class="team-img-holder">
+                                <a href="#"><img src="img/team/2.png" alt="team" class="img-responsive"></a>
+                            </div>
+                            <div class="team-content-holder">
+                                <h3><a href="#">Jack Sparo</a></h3>
+                                <p>Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="expert-team-box">
+                            <ul class="expert-team-social">
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-pinterest"></i></a></li>
+                            </ul>
+                            <div class="team-img-holder">
+                                <a href="#"><img src="img/team/3.png" alt="team" class="img-responsive"></a>
+                            </div>
+                            <div class="team-content-holder">
+                                <h3><a href="#">Tina Zena</a></h3>
+                                <p>Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="expert-team-box">
+                            <ul class="expert-team-social">
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i aria-hidden="true" class="fa fa-pinterest"></i></a></li>
+                            </ul>
+                            <div class="team-img-holder">
+                                <a href="#"><img src="img/team/4.png" alt="team" class="img-responsive"></a>
+                            </div>
+                            <div class="team-content-holder">
+                                <h3><a href="#">Kazi Fahim</a></h3>
+                                <p>Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Expert Team Area End Here -->
+        <!-- Advantage2 Area Start Here -->
+        <div class="advantage2-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="advantage-area-box">
+                            <div class="media">
+                                <a class="pull-left" href="#">
+                                    <i class="flaticon-truck"></i>
+                                </a>
+                                <div class="media-body">
+                                    <h3>FREE SHIPPING</h3>
+                                    <p>On All Orders</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="advantage-area-box">
+                            <div class="media">
+                                <a class="pull-left" href="#">
+                                    <i class="flaticon-headphones"></i>
+                                </a>
+                                <div class="media-body">
+                                    <h3>24/7 SERVICE</h3>
+                                    <p>Get Help When You Need</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="advantage-area-box">
+                            <div class="media">
+                                <a class="pull-left" href="#">
+                                    <i class="flaticon-reload"></i>
+                                </a>
+                                <div class="media-body">
+                                    <h3>100% MONEY BACK</h3>
+                                    <p>Within 30 Day Guarantee</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Advantage2 Area End Here -->
+        @stop
