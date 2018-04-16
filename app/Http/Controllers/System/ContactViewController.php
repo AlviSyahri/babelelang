15e5829033256150ad6cedd0e4c358ef6f13b64a
Felix Winangun <felixwinangun@ymail.com>
2018-04-16 16:27:02 +0700

    pindahin contact dari admin ke user

--- app/Http/Controllers/System/ContactViewController.php
+++ app/Http/Controllers/System/ContactViewController.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
 class ContactViewController extends Controller
 {
     public function viewcontact(){
-    	return view('admin/contact');
+    	return view('user/contact');
     }
 
 }
